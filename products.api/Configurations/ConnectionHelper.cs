4503d06bae470c699f762070c2441dd18ee9b2a6
Renato Santos <renato.santos@modalgr.com.br>
2024-05-05 23:47:52 -0300

    fix: corrigindo conexao com o container interno do docker

--- products.api/Configurations/ConnectionHelper.cs
+++ products.api/Configurations/ConnectionHelper.cs
@@ -6,9 +6,20 @@ public static class ConnectionHelper
     {
         public static string GetConnectionString(IConfiguration configuration)
         {
+            //DOCKER
+            var isRunningInDocker = configuration.GetSection("DockerContainer")["isRunningInDockerContainer"] == "True" ? true : false; 
+            if(isRunningInDocker) return DockerContainerConnectionString(configuration);
+
+            //LOCAL
             var connectionString = configuration.GetConnectionString("PostgressDefaultConnectionDEV");
             var databaseUrl = System.Environment.GetEnvironmentVariable("DATABASE_URL");
             return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+
+        private static string DockerContainerConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            return connectionString;
         }
 
         //build the connection string from the environment. i.e. Heroku
